2e873f493ae42fe17d8a97461946ad6463e3c074
Ravit-Hayden <hayden@ravit.team>
2025-08-30 10:48:11 +0900

    docs: git 커밋 및 푸시 자동화 스크립트 메모리 업데이트

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="min-h-screen bg-black text-white flex items-center justify-center">
 	<div class="text-center">
-		<img src="/logo.svg" alt="Sugar Rush!" class="logo" />
+		<img src="/logo.svg" alt="Sugar Rush" class="logo" />
 	</div>
 </div>
 
